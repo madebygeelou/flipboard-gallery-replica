1385522a49189f01080c0e932c445374ff3bf4dd
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-04-30 22:03:15 -0700

    finished

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 function ArtArticle() {
   return (
     <Card style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top" src={Gallery} /> */}
+      <Card.Img variant="top" src='https://www.artic.edu/iiif/2/e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9/full/843,/0/default.jpg'/>
     </Card>
   );
 }
